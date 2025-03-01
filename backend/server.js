4f84e8fd5fcaadc990709b45b85d214587766eae
Balaji <balajijayakumar777@gmail.com>
2025-03-01 11:32:48 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const http = require('http');   // Import Node.js core module
+const app = require('./app'); // Import the Express app
+const port = process.env.PORT || 3000; // Set the port to either the environment port or 3000
+
+const server = http.createServer(app); // Create an HTTP server and pass the Express app as the request/response handler
+
+
+server.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`);
+}); 
